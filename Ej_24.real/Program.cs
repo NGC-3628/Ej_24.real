169f61de132550c7c2c17eebfd5ed95f4a0f06b2
isaacvgn@gmail.com <itu22002@byui.edu>
2024-11-30 20:31:47 -0600

    Add project files.

---
+++ Ej_24.real/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_24.real
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
